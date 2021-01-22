2d6d608736f2678fb2b34eefb987cce3511f2399
TruongL <5490853+qcbit@users.noreply.github.com>
2021-01-22 09:42:31 -0600

    pointer to array

---
+++ c_scratch/array_ptr_addr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define SIZE 5
+int main(int argc, char **argv)
+{
+    int arr[SIZE];
+    arr[0] = 9;
+    arr[1] = 22;
+    arr[2] = 30;
+    arr[3] = 23;
+    arr[4] = 18;
+
+    printf("int* ptr = &arr[0]")
+    int* ptr = &arr[0];
+    for (;;)
+    {
+        printf("%d\n", *ptr);
+        if (ptr == &arr[SIZE - 1])
+        {
+            break;
+        }
+        ptr++;
+    }
+
+    printf("ptr = &*arr")
+    ptr = &*arr;
+    for (;;)
+    {
+        printf("%d\n", *ptr);
+        if (ptr == &arr[SIZE - 1])
+        {
+            break;
+        }
+        ptr++;
+    }
+
+    printf("ptr = &arr")
+    ptr = &arr;
+    for (;;)
+    {
+        printf("%d\n", *ptr);
+        if (ptr == &arr[SIZE - 1])
+        {
+            break;
+        }
+        ptr++;
+    }
+    return 0;
+}
