913816ba08dd657b6c47f77e4cbdcabfae19ee70
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-08 07:41:22 -0500

    Refactored the test suite

--- go/testing/hello/hello_test.go
+++ go/testing/hello/hello_test.go
@@ -4,21 +4,23 @@ import "testing"
 
 // TestHello is a suite of tests for the Hello function
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("Chris")
-		want := "Hello, Chris"
 
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
