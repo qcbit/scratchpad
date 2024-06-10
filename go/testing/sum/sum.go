8d33eb1d00c8f72bcf798140d5b59879b2c7d61f
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-10 06:59:29 -0500

    Working with slices

--- go/testing/sum/sum.go
+++ go/testing/sum/sum.go
@@ -23,3 +23,56 @@ func Sum3(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbers [][]int) int {
+	var sum int
+	for _, numbersSlice := range numbers {
+		sum += Sum3(numbersSlice)
+	}
+	return sum
+}
+
+func SumAll2(numbers ...[]int) int {
+	var sum int
+	for _, numbersSlice := range numbers {
+		sum += Sum3(numbersSlice)
+	}
+	return sum
+}
+
+// SumAll3 is a variadic function that takes a slice of slices of integers and returns a slice of integers.
+// This function takes a performance hit because it creates a new slice for each iteration.
+func SumAll3(numbers ...[]int) []int {
+	var sums []int
+	for _, numbersSlice := range numbers {
+		sums = append(sums, Sum3(numbersSlice))
+	}
+	return sums
+}
+
+// SumAll4 is a variadic function that takes a slice of slices of integers and returns a slice of integers.
+// This function is more performant than SumAll3 because it pre-allocates the slice.
+func SumAll4(numbers ...[]int) []int {
+	sums := make([]int, len(numbers)) // pre-allocate the slice
+	for i, numbersSlice := range numbers {
+		sums[i] = Sum3(numbersSlice)
+	}
+	return sums
+}
+
+// SumAllTails is a variadic function that takes a slice of slices of integers and returns a slice of integers.
+// It sums the tails of the slices.
+func SumAllTails(numbers ...[]int) []int {
+	sums := make([]int, len(numbers))
+	for i := range numbers {
+		sliceLen := len(numbers[i])
+		if sliceLen == 0 {
+			sums[i] = 0
+		} else if sliceLen == 1 {
+			sums[i] = numbers[i][0]
+		} else {
+			sums[i] = Sum3(numbers[i][1:])
+		}
+	}
+	return sums
+}
--- go/testing/sum/sum_test.go
+++ go/testing/sum/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := [5]int{1, 2, 3, 4, 5}    // fixed size array
@@ -37,5 +40,79 @@ func TestSum(t *testing.T) {
 		got := Sum3(numbers4)
 		want := 6
 		assertCorrect(t, got, want, numbers4)
+	})
+
+	t.Run("sum all slices of any size", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3}
+		numbers2 := []int{1, 2, 3, 4, 5}
+		numbers := [][]int{numbers1, numbers2}
+		got := SumAll(numbers)
+		want := 21
+		assertCorrect(t, got, want, numbers)
+	})
+
+	t.Run("sum all slices of any size 2", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3}
+		numbers2 := []int{1, 2, 3, 4, 5}
+		numbers3 := []int{1, 2, 3, 4, 5, 6}
+		numbers := [][]int{numbers1, numbers2, numbers3}
+		got := SumAll2(numbers1, numbers2, numbers3)
+		want := 42
+		assertCorrect(t, got, want, numbers)
+	})
+
+	t.Run("sum all slices of any size 3", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3}
+		numbers2 := []int{1, 2, 3, 4, 5}
+		numbers3 := []int{1, 2, 3, 4, 5, 6}
+		got := SumAll3(numbers1, numbers2, numbers3)
+		want := []int{6, 15, 21}
+		// Go does not allow us to compare slices directly. We need to use the reflect.DeepEqual function.
+		// The reflect package is part of the Go standard library and provides a way to look at the type of an interface object at runtime.
+		// The reflect.DeepEqual is not type safe and will compile even if we pass in the wrong types.
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("sum all slices of any size 4", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3}
+		numbers2 := []int{1, 2, 3, 4, 5}
+		numbers3 := []int{1, 2, 3, 4, 5, 6}
+		got := SumAll4(numbers1, numbers2, numbers3)
+		want := []int{6, 15, 21}
+		// Go does not allow us to compare slices directly. We need to use the reflect.DeepEqual function.
+		// The reflect package is part of the Go standard library and provides a way to look at the type of an interface object at runtime.
+		// The reflect.DeepEqual is not type safe and will compile even if we pass in the wrong types.
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	assertCorrect := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("1 element slices", func(t *testing.T) {
+		got := SumAllTails([]int{1}, []int{3,4,5})
+		want := []int{1, 9}
+		assertCorrect(t, got, want)
 	})
 }
