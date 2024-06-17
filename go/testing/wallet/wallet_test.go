bad94cce831479550b7d60f35d43a09a0a8a2cb9
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-17 18:18:00 -0500

    Assert no error

--- go/testing/wallet/wallet_test.go
+++ go/testing/wallet/wallet_test.go
@@ -5,33 +5,39 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	
-	t.Run("Deposit", func(t *testing.T){
+
+	t.Run("Deposit", func(t *testing.T) {
 		var wallet Wallet
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10.0))
 	})
-	
+
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, wallet.Withdraw(Bitcoin(10)))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
-	
+
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{startingBalance}
 		assertError(t, wallet.Withdraw(Bitcoin(100)), ErrInsufficientFunds)
-
 		assertBalance(t, wallet, startingBalance)
 	})
 }
 
-func assertBalance(t *testing.T, wallet Wallet, want Bitcoin){
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
 	t.Helper()
 	got := wallet.Balance()
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
