e30808a55ff2fa4d150f5a3c1e623004e2e13f31
qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-18 22:43:28 +0000

    structs

--- go/testing/structs/models.go
+++ go/testing/structs/models.go
@@ -1,25 +1,25 @@
 package learningStructs
 
 type Shape interface {
-  Area() float64
+	Area() float64
 }
 
 type Rectangle struct {
-  Length float64
-  Width  float64
+	Length float64
+	Width  float64
 }
 
 type Point struct {
-  X float64
-  Y float64
+	X float64
+	Y float64
 }
 
 type Circle struct {
-  Point  Point
-  Radius float64
+	Point  Point
+	Radius float64
 }
 
 type Triangle struct {
-  Base float64
-  Height float64
+	Base   float64
+	Height float64
 }
--- go/testing/structs/perimeter.go
+++ go/testing/structs/perimeter.go
@@ -3,7 +3,7 @@ package learningStructs
 import "math"
 
 func (rect Rectangle) Perimeter() float64 {
-	return 2*(rect.Length + rect.Width)
+	return 2 * (rect.Length + rect.Width)
 }
 
 func (circ Circle) Perimeter() float64 {
