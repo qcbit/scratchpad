5af4c3349f0c3130bb5499830d261578e4191ae6
qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-18 23:30:00 +0000

    errors

--- go/testing/maps/dictionary_test.go
+++ go/testing/maps/dictionary_test.go
@@ -4,9 +4,21 @@ import "testing"
 
 func TestSearch(t *testing.T) {
   dictionary := Dictionary{"test": "this is just a test"}
-  got := dictionary.Search("test")
-  want := "this is just a test"
-  assertString(t, got, want)
+  
+  t.Run("known word", func(t *testing.T){
+    got, _ := dictionary.Search("test")
+    want := "this is just a test"
+    assertString(t, got, want)  
+  })
+  
+  t.Run("Unknown word", func(t *testing.T){
+    _, err := dictionary.Search("unknown")
+    want := "could not find the word you were looking for"
+    if err == nil {
+      t.Fatal("expected to get an error")
+    }
+    assertString(t, err.Error(), want)
+  })
 }
 
 func assertString(t *testing.T, got, want string) {
