77ba71ccab38e285c047bfc92b4427305328254d
qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-18 23:10:27 +0000

    testing maps

---
+++ go/testing/maps/dictionary_test.go
@@ -0,0 +1,17 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+  dictionary := map[string]string{"test": "this is just a test"}
+  got := Search(dictionary, "test")
+  want := "this is just a test"
+  assertString(t, got, want)
+}
+
+func assertString(t *testing.T, got, want string) {
+  t.Helper()
+  if got != want {
+    t.Errorf("got %q want %q", got, want)
+  }
+}
