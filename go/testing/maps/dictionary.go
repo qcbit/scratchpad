c08aa2078783b5867164181dfc73c697dd9a00cf
qcbit <5490853+qcbit@users.noreply.github.com>
2024-06-19 00:37:30 +0000

    refactored errors

--- go/testing/maps/dictionary.go
+++ go/testing/maps/dictionary.go
@@ -2,12 +2,14 @@ package dictionary
 
 import "errors"
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
   def, ok := d[word]
   if !ok {
-    return "", errors.New("could not find the word you were looking for")
+    return "", ErrNotFound
   }
   return def, nil
 }
--- go/testing/maps/dictionary_test.go
+++ go/testing/maps/dictionary_test.go
@@ -12,12 +12,8 @@ func TestSearch(t *testing.T) {
   })
   
   t.Run("Unknown word", func(t *testing.T){
-    _, err := dictionary.Search("unknown")
-    want := "could not find the word you were looking for"
-    if err == nil {
-      t.Fatal("expected to get an error")
-    }
-    assertString(t, err.Error(), want)
+    _, got := dictionary.Search("unknown")
+    assertError(t, got, ErrNotFound)
   })
 }
 
@@ -25,5 +21,12 @@ func assertString(t *testing.T, got, want string) {
   t.Helper()
   if got != want {
     t.Errorf("got %q want %q", got, want)
+  }
+}
+
+func assertError(t *testing.T, got, want error) {
+  t.Helper()
+  if got != want {
+    t.Errorf("got error %q want %q", got, want)
   }
 }
