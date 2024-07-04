76ca256943a21746331c542d22c9a140a169c166
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-07-04 10:27:58 -0500

    Added plotting

--- python/linear_regression/linear_regression.py
+++ python/linear_regression/linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([2, 4, 5, 4, 5])
@@ -10,3 +11,11 @@ m = np.sum((x - mean_x) * (y - mean_y)) / np.sum((x - mean_x) ** 2 )
 c = mean_y - m * mean_x
 
 print(f"y = {m}x + {c}")
+
+plt.scatter(x, y, color = "red", marker="o", s=30)
+y_pred = m * x + c
+plt.plot(x, y_pred, color="blue")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Simple Linear Regression")
+plt.show()
