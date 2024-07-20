6d96bf9996f95f7f16e4cb8f6c340c4e16879232
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-07-20 18:22:28 -0500

    Process string array

---
+++ c_sharp/CsharpProjects/ProcessStrAry/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+int periodLocation = 0;
+for(int ndx = 0; ndx < myStrings.Length; ndx++) {
+  string str = myStrings[ndx];
+  periodLocation = str.IndexOf('.');
+  while (periodLocation > 0) {
+    Console.WriteLine(str.Substring(0, periodLocation));
+    str = str.Remove(0, periodLocation + 1);
+    str = str.TrimStart(); // Removes leading whitespace
+    periodLocation = str.IndexOf('.');
+  }
+  Console.WriteLine(str);
+}
