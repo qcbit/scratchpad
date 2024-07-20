b20db19c82809002d40cb967044438e86b3ada6a
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-07-20 17:46:19 -0500

    Validate string input

---
+++ c_sharp/CsharpProjects/ValidStrInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+string? input;
+bool valid = false;
+do {
+  Console.Write("Enter a role [Administrator, Manager, User]: ");
+  input = Console.ReadLine();
+  if (input is not null) {
+    valid = input.ToLower().Trim() == "administrator" || input.ToLower().Trim() == "manager" || input.ToLower().Trim() == "user";
+    if (! valid) {
+      Console.WriteLine($"The role name that you entered, \"{input}\" is not valid.");
+    }
+  }
+} while (! valid);
+
+Console.Write($"The input value ({input}) is accepted.");
