1c1f18b3f59ada6a7a73538821d41f1fc402e2e6
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-08-20 19:32:32 -0500

    Bubble Sort

--- go/sort/bubbleSort.go
+++ go/sort/bubbleSort.go
@@ -5,15 +5,15 @@ func BubbleSort(A []int) []int {
 	if len(A) == 0 {
 		return A
 	}
-    
-    n := len(A)
-    for i := 0; i < len(A); i++ {
-    	for j := n; j > i; j-- {
-    		if A[j-1] < A[j] {
-    			A[j-1], A[j] = A[j], A[j-1]
-    		}
-    	}
-    }
+
+	n := len(A)
+	for i := 0; i < len(A); i++ {
+		for j := n - 1; j > i; j-- {
+			if A[j-1] > A[j] {
+				A[j-1], A[j] = A[j], A[j-1]
+			}
+		}
+	}
 
 	return A
 }
