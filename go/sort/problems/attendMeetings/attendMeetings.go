6d2fe7caec2b8559d0aacde25e7191a4dba9849c
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-08-28 21:01:25 -0500

    Added problem to attend meetings

---
+++ go/sort/problems/attendMeetings/attendMeetings.go
@@ -0,0 +1,42 @@
+// Problem
+
+// Attend Meetings
+// Given a list of meeting intervals where each interval consists of a start and an end time, check if a person can attend all the given meetings such that only one meeting can be attended at a time.
+
+// Example One
+// {
+// "intervals": [
+// [1, 5],
+// [5, 8],
+// [10, 15]
+// ]
+// }
+// Output:
+
+// 1
+// As the above intervals are non-overlapping intervals, it means a person can attend all these meetings.
+
+// Example Two
+// {
+// "intervals": [
+// [1, 5],
+// [4, 8]
+// ]
+// }
+// Output:
+
+// 0
+// Time 4 - 5 is overlapping in the first and second intervals.
+
+// Notes
+// A new meeting can start at the same time the previous one ended.
+// Constraints:
+
+// 1 <= number of intervals <= 105
+// 0 <= start time < end time <= 109
+package attendmeetings
+
+func can_attend_all_meetings(intervals [][]int) int {
+	// Write your code here.
+	return 0
+}
