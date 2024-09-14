579dd31a73ccdc40159dfa0609a9ed0f9cfd8c2b
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-09-14 16:18:48 -0500

    Renamed

--- go/trees/problems/insertInBST/insertInBST.go
+++ go/trees/problems/insertInBST/insertInBST.go
@@ -1,5 +1,5 @@
 // Problem
-  
+
 // Insert In BST
 // Given an array of numbers, build a binary search tree(BST) by inserting the values sequentially inside an initially empty BST.
 
@@ -31,48 +31,48 @@
 // 1 <= length of the input array <= 103
 // -104 <= values in the array <= 104
 // All of the given values are distinct.
-package insert
+package insertinbst
 
 type BinaryTreeNode struct {
-    value int
-    left *BinaryTreeNode
-    right *BinaryTreeNode
+	value int
+	left  *BinaryTreeNode
+	right *BinaryTreeNode
 }
 
-func build_a_bst(values [] int) *BinaryTreeNode {
+func build_a_bst(values []int) *BinaryTreeNode {
 	if len(values) == 0 {
-			return &BinaryTreeNode{}
+		return &BinaryTreeNode{}
 	}
 	root := &BinaryTreeNode{values[0], nil, nil}
 	for i := 1; i < len(values); i++ {
-		 insert(root, values[i]) 
+		insert(root, values[i])
 	}
 	return root
 }
-	
+
 func insert(node *BinaryTreeNode, k int) *BinaryTreeNode {
-	newNode := &BinaryTreeNode{value:k}
+	newNode := &BinaryTreeNode{value: k}
 	if node == nil {
-			return newNode
+		return newNode
 	}
 	var prev *BinaryTreeNode
 	curr := node
 	for curr != nil {
-			if k == curr.value {
-					return node
-			}
-			prev = curr
-			if k < curr.value {
-					curr = curr.left
-			} else {
-					curr = curr.right
-			}
+		if k == curr.value {
+			return node
+		}
+		prev = curr
+		if k < curr.value {
+			curr = curr.left
+		} else {
+			curr = curr.right
+		}
 	}
-	
+
 	if k < prev.value {
-			prev.left = newNode
+		prev.left = newNode
 	} else {
-			prev.right = newNode
+		prev.right = newNode
 	}
 
 	return node
