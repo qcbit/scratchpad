a697e36c8cf64bdfe967725a872ddf0b0ba2a7dd
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-10-11 11:35:21 -0500

    added test

--- go/problems/reversePolishNotation/reversePolishNotation_test.go
+++ go/problems/reversePolishNotation/reversePolishNotation_test.go
@@ -9,16 +9,17 @@ func TestAdder(t *testing.T) {
 	calc := NewCalculator(20)
 	tests := []struct {
 		rpnString string
-		want float64
+		want      float64
 	}{
 		{"4 3 - 5 *", 5},
 		{"3 5 * 4 -", 11},
 		{"15 3 /", 5},
 		{"-21 3 /", -7},
 		{"2 5 - 5 *", -15},
+		{"3 -10 +", -7},
 	}
 
-	t.Run("Reverse Polish Notation Tests", func(t *testing.T){
+	t.Run("Reverse Polish Notation Tests", func(t *testing.T) {
 		for _, tt := range tests {
 			ans := calc.calculate(tt.rpnString)
 			if ans != tt.want {
