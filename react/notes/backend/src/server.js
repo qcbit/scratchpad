2cd3b98b40991f86be078fef92e13a44fef6b8c2
qcbit <5490853+qcbit@users.noreply.github.com>
2025-06-15 15:36:01 -0500

    Added connection to MongoDB, Mongoose, and model

--- react/notes/backend/src/server.js
+++ react/notes/backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import notesRoutes from '../routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:${PORT}");
 });
