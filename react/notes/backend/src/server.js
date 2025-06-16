db20fa971ecb888c26b5f12792f3c89caac4796a
qcbit <5490853+qcbit@users.noreply.github.com>
2025-06-16 18:32:56 -0500

    Added dependencies. Updated for CORS

--- react/notes/backend/src/server.js
+++ react/notes/backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
@@ -11,6 +13,11 @@ const PORT = process.env.PORT || 3000;
 
 
 // Middleware
+app.use(cors(
+  {
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  }
+));
 app.use(express.json());
 app.use(rateLimiter);
 
